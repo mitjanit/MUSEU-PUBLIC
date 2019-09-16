5b894acf1cca9a62d53aeb6a45708218a003fba5
profeweb <42413333+mitjanit@users.noreply.github.com>
2019-09-16 11:23:50 +0200

    BUILD

---
+++ build/precache-manifest.ef7a11d0fa0fb17954a1052e7e7ad355.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be526aac2f62f1d4dcb88bcc1d43af86",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b850c52b54f6359beda",
+    "url": "/static/css/main.5bcfc507.chunk.css"
+  },
+  {
+    "revision": "08f410f49c77ef3cabe8",
+    "url": "/static/js/2.398b356a.chunk.js"
+  },
+  {
+    "revision": "4b850c52b54f6359beda",
+    "url": "/static/js/main.29ed4bba.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);

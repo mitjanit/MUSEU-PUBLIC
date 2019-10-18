cd62fca2e79d59bb505b8ae17b6e9c4b2ab45964
profeweb <42413333+mitjanit@users.noreply.github.com>
2019-10-18 12:07:14 +0200

    Constants API

--- src/Constants.js
+++ src/Constants.js
@@ -2,5 +2,19 @@
 const URL_API = "http://34.90.211.155/api/";
 
 var dict = {};
-dict['Menorca']=52;
+
+dict['Mallorca']=7;
+dict['Menorca']=51;
+dict['Eivissa']=52;
+
+dict['Àlava']=1;
+dict['Albacete']=2;
+dict['Alicante']=3;
+dict['Almería']=4;
+dict['Ávila']=5;
+dict['Badajoz']=6;
+dict['Barcelona']=8;
+dict['Burgos']=9;
+dict['Cáceres']=10;
+
 

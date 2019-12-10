b14a86218b3aa710a76f8674130b43496776dace
profeweb <42413333+mitjanit@users.noreply.github.com>
2019-12-10 17:39:36 +0100

    Build Jugadors

---
+++ build/precache-manifest.ec98e096bb05a3d09a6555e96905ca82.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d37e212323a454fc9b79d9773ca8f68",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1f126e2fb691aba9055c",
+    "url": "/static/css/main.cee37af5.chunk.css"
+  },
+  {
+    "revision": "bc38b7a535bb6fb9bd64",
+    "url": "/static/js/2.df8204c6.chunk.js"
+  },
+  {
+    "revision": "1f126e2fb691aba9055c",
+    "url": "/static/js/main.eb0268c4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
